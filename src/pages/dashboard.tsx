25a6b76ceccf51cba2274bf9e05751938d078327
lucasedusi <lucasmto.16@hotmail.com>
2022-05-30 15:35:59 -0300

    create aside component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,15 @@
 import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { SideBar } from "../components/SideBar";
 
 export default function Dashboard() {
 	return (
 		<Flex direction="column" h="100vh">
 			<Header />
+
+			<Flex w="100%" my="6" maxWidth={1250} px="6">
+				<SideBar />
+			</Flex>
 		</Flex>
 	);
 }
