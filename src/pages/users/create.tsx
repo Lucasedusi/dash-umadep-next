3d870f5e147549ef9389b687d1a8e5cc02278a89
lucasedusi <lucasmto.16@hotmail.com>
2022-06-01 20:29:14 -0300

    validade user form

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -13,7 +13,41 @@ import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { SideBar } from "../../components/SideBar";
 
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type CreateUserFormData = {
+	name: string;
+	email: string;
+	password: string;
+	password_confimation: string;
+};
+
+const signInFormSchema = yup.object().shape({
+	name: yup.string().required("Nome Obrigatório"),
+	email: yup.string().required("E-mail Obrigatório").email("E-mail Inválido"),
+	password: yup.string().required("Senha Obrigatória").min(6, "Tamanho Mínimo"),
+	password_confimation: yup
+		.string()
+		.oneOf([null, yup.ref("password")], "Precisam ser iguais"),
+});
+
 export default function UserCreaete() {
+	const { register, handleSubmit, formState } = useForm({
+		resolver: yupResolver(signInFormSchema),
+	});
+
+	const { errors } = formState;
+
+	const handleUserCreate: SubmitHandler<CreateUserFormData> = async (
+		values
+	) => {
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+
+		console.log(values);
+	};
+
 	return (
 		<Box>
 			<Header />
@@ -21,7 +55,14 @@ export default function UserCreaete() {
 			<Flex w="100%" my="10" maxWidth={1250} mx="auto" px="6">
 				<SideBar />
 
-				<Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+				<Box
+					as="form"
+					flex="1"
+					borderRadius={8}
+					bg="gray.800"
+					p={["6", "8"]}
+					onSubmit={handleSubmit(handleUserCreate)}
+				>
 					<Heading size="lg" fontWeight="normal">
 						Criar Usuário
 					</Heading>
@@ -30,15 +71,35 @@ export default function UserCreaete() {
 
 					<VStack spacing="8">
 						<SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-							<Input name="name" label="Nome Completo" />
-							<Input name="email" type="email" label="E-mail" />
+							<Input
+								name="name"
+								type="name"
+								label="Nome Completo"
+								error={errors.name}
+								{...register("name")}
+							/>
+							<Input
+								name="email"
+								type="email"
+								label="E-mail"
+								error={errors.email}
+								{...register("email")}
+							/>
 						</SimpleGrid>
 						<SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-							<Input name="password" type="password" label="Senha" />
+							<Input
+								name="password"
+								type="password"
+								label="Senha"
+								error={errors.password}
+								{...register("password")}
+							/>
 							<Input
 								name="password_confirmation"
 								type="password"
 								label="Confirmar Senha"
+								error={errors.password_confimation}
+								{...register("password_confimation")}
 							/>
 						</SimpleGrid>
 					</VStack>
@@ -48,7 +109,14 @@ export default function UserCreaete() {
 							<Link href="/users">
 								<Button colorScheme="whiteAlpha">Cancelar</Button>
 							</Link>
-							<Button colorScheme="pink">Salvar</Button>
+
+							<Button
+								type="submit"
+								colorScheme="pink"
+								isLoading={formState.isSubmitting}
+							>
+								Salvar
+							</Button>
 						</HStack>
 					</Flex>
 				</Box>
