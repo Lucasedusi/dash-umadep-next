570f54ae8b2fd7ae19a671412fbba01499951a91
lucasedusi <lucasmto.16@hotmail.com>
2022-06-02 14:38:04 -0300

    add isFetching

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,8 +16,7 @@ import {
 	useBreakpointValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
@@ -25,7 +24,7 @@ import { SideBar } from "../../components/SideBar";
 import { useQuery } from "react-query";
 
 export default function UserList() {
-	const { data, isLoading, error } = useQuery("users", async () => {
+	const { data, isLoading, isFetching, error } = useQuery("users", async () => {
 		const response = await fetch("http://localhost:3000/api/users");
 		const data = await response.json();
 
@@ -61,6 +60,9 @@ export default function UserList() {
 					<Flex mb="8" justify="space-between" align="center">
 						<Heading size="lg" fontWeight="normal">
 							Usuário
+							{!isLoading && isFetching && (
+								<Spinner size="sm" color="gray.500" ml="4" />
+							)}
 						</Heading>
 
 						<Link href="users/create">
